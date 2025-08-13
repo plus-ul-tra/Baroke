706c40d895043e8387160a01038978d46ba71256
DanJi-NaRi <bjh000523@naver.com>
2025-08-13 10:18:27 +0900

    플레이어 수치 조절

--- Engine/PlayerInfo.h
+++ Engine/PlayerInfo.h
@@ -6,7 +6,7 @@ struct PlayerInfo
 	int m_blackStoneUpgrade = 0; // Èæµ¹ ¾÷±×·¹ÀÌµå È½¼ö
 	int m_tempBlackStone = 0;
 	int m_whiteLeft = 0;
-	int m_money = 100;
+	int m_money = 6;
 	int m_rerollCount = 0; // »óÁ¡ ¸®·Ñ È½¼ö
 	int m_waxMoney = 0;
 	int m_score = 0;
@@ -21,7 +21,7 @@ struct PlayerInfo
 	void decBlackCount(int count) { m_tempBlackStone -= count; }
 	void ResetRound() { m_tempBlackStone = 0; }
 	void initGame() {
-		m_BlackStone = 12;
+		m_BlackStone = 20;
 		m_blackStoneUpgrade = 0;
 		m_tempBlackStone = 0;
 		m_whiteLeft = 0;
